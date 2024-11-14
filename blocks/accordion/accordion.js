bd1b1ce78d57438184439336405b87a2ecd04f1f
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-14 13:12:56 +0530

    mob

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -28,7 +28,7 @@ export default function decorate(block) {
 
   const searchPath = '/blocks/accordion';
   console.log("searchPath"+searchPath);
-  console.log("code "+${window.hlx.codeBasePath});
+  console.log("code "+window.hlx.codeBasePath);
   const resp = await fetch(`${window.hlx.codeBasePath}${searchPath}/accordion.html`, {
     signal: AbortSignal.timeout(FETCH_TIMEOUTS.default),
   });
