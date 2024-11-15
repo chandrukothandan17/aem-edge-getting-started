dc124f63773963bdd49dcade42f0b85477c4e494
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-15 17:20:49 +0530

    hsdf

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,5 +1,5 @@
 console.log("hello");
 const serviceAlertItem = document.querySelectorAll('.service-alert-wrapper div[data-aue-model="service-alert-item"]'); 
 serviceAlertItem.forEach(function(items) { 
-    items.firstChild.innerHtml += `<p data-aue-prop="alert-id" data-aue-label="Alert Id" data-aue-type="text">`+ Math.floor(Math.random() * 999999) +`</p>`;
+    items.firstChild.innerHtml = `<p data-aue-prop="alert-id" data-aue-label="Alert Id" data-aue-type="text">`+ Math.floor(Math.random() * 999999) +`</p>`;
 }); 
