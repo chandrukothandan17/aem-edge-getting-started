237eb37ecf31d8aba2a44682175cc76a25918bd6
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-15 17:10:18 +0530

    child

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,5 +1,5 @@
 console.log("hello");
 const serviceAlertItem = document.querySelectorAll('.service-alert-wrapper div[data-aue-model="service-alert-item"]'); 
 serviceAlertItem.forEach(function(items) { 
-    console.log(items.data-aue-label);
+    console.log(items.children);
 }); 
