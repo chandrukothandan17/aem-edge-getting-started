488f73f3ba898f3dba87ab41ee056036580ceb6b
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-15 17:19:16 +0530

    Dfdd

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,10 +1,5 @@
 console.log("hello");
 const serviceAlertItem = document.querySelectorAll('.service-alert-wrapper div[data-aue-model="service-alert-item"]'); 
 serviceAlertItem.forEach(function(items) { 
-    const pele = document.createElement('p');
-    pele.data-aue-prop = 'alert-id';
-    pele.data-aue-label = 'Alert Id';
-    pele.data-aue-type = 'text';
-    pele.textContent = 'alert'+Math.floor(Math.random() * 999999);
-    items.firstChild.append(pele);
+    items.firstChild.innerHtml += `<p data-aue-prop="alert-id" data-aue-label="Alert Id" data-aue-type="text">`+ Math.floor(Math.random() * 999999) +`</p>`;
 }); 
