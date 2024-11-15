1e850f93f561c9873f0ae176d431956ddb5c8ac7
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-15 17:05:21 +0530

    dfd

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,5 +1,7 @@
 console.log("hello");
 const serviceAlertItem = document.querySelectorAll('.service-alert-wrapper div[data-aue-model="service-alert-item"]'); 
 serviceAlertItem.forEach(function(items) { 
-    console.log(items); 
+    items.forEach(function(it){
+        console.log(it);
+    });
 }); 
