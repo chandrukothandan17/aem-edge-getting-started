b28a4eceaf015cee0d6eae31732180c20c13595d
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-15 17:14:23 +0530

    pele

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,5 +1,10 @@
 console.log("hello");
 const serviceAlertItem = document.querySelectorAll('.service-alert-wrapper div[data-aue-model="service-alert-item"]'); 
 serviceAlertItem.forEach(function(items) { 
-    console.log(items.children);
+    const pele = document.createElement('p');
+    pele.data-aue-prop = 'alert-id';
+    pele.data-aue-label = 'Alert Id';
+    pele.data-aue-type = 'text';
+    pele.textContent = 'alert'+Math.floor(Math.random() * 999999);
+    items.firstChild.append(pele);
 }); 
