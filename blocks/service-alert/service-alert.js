c643080afd6c805a2d17f5c742bb62d53300aafd
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-15 17:06:39 +0530

    Ni

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,7 +1,5 @@
 console.log("hello");
 const serviceAlertItem = document.querySelectorAll('.service-alert-wrapper div[data-aue-model="service-alert-item"]'); 
 serviceAlertItem.forEach(function(items) { 
-    items.forEach(function(it){
-        console.log(it);
-    });
+    console.log(items.data-aue-label);
 }); 
