a7f4c0c0027de45d8572cfeba9c1f5093844cab4
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-18 10:25:16 +0530

    click

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,7 +1,2 @@
 console.log("hello");
-const serviceAlertItem = document.querySelectorAll('.service-alert-wrapper div[data-aue-model="service-alert-item"]'); 
-serviceAlertItem.forEach(function(items) { 
-    items.firstChild.innerHtml += `<p data-aue-prop="alert-id" data-aue-label="Alert Id" data-aue-type="text">`+ Math.floor(Math.random() * 999999) +`</p>`;
-    console.log(items.firstChild.innerHtml);
-    console.log(items.firstChild);
-}); 
+$(".Dniwja_spectrum-ActionButton").click(function(){  console.log("test"); });
