e8023791908c431a2b0b54284e9faa22cb16eefe
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-18 17:31:41 +0530

    Test

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,1 +1,20 @@
 console.log("hello");
+
+fetch('/content/aem-edge-getting-started-site/index/service-alerts/jcr:content/root/section.infinity.json', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
