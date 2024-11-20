33c529dfdb61634589db1e0168611c9fec3033b6
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-20 21:52:33 +0530

    list

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -2,5 +2,11 @@ console.log("hello");
 
 document.addEventListener('DOMContentLoaded', function() {
     const wrapper = document.getElementsByClassName('service-alert-wrapper'); 
-    console.log(wrapper[0]);
+    wrapper.forEach((ele) => {
+        const firstChild = ele.firstElementChild;
+        console.log(firstChild);
+        firstChild.forEach((list) => {
+              console.log(list);
+        });
+    });
 });
