c77a80709f3b0f5cde3dbc4008bc49d0fb227796
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-20 22:00:17 +0530

    loading

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,17 +1,18 @@
 console.log("hello");
 
-if (document.readyState != "loading") { console.log("loading done");}
+if (document.readyState != "loading") { 
+  console.log("loading done");
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("dom loaded");
-    const wrapper = document.getElementsByClassName('service-alert-wrapper'); 
-    console.log(wrapper);
-    wrapper.forEach((ele) => {
-        console.log(ele);
-        const firstChild = ele.firstElementChild;
-        console.log(firstChild);
-        firstChild.forEach((list) => {
-              console.log(list);
-        });
-    });
-});
+  console.log("dom loaded");
+  const wrapper = document.getElementsByClassName('service-alert-wrapper'); 
+  console.log(wrapper);
+  wrapper.forEach((ele) => {
+      console.log(ele);
+      const firstChild = ele.firstElementChild;
+      console.log(firstChild);
+      firstChild.forEach((list) => {
+            console.log(list);
+      });
+  });
+
+}
