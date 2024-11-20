ef1919c867ab4e7b9594edac9faa6f283f4fff1d
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-20 21:37:11 +0530

    HI

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,8 +1,12 @@
 console.log("hello");
 
 
-const buttons = document.querySelectorAll('button[aria-label="Properties"]');
-console.log(buttons.length);
-buttons[0].addEventListener('click', function() {
-  console.log("propertied");
-});
+async function propertiesClicked(){
+  const buttons = document.querySelectorAll('button[aria-label="Properties"]');
+  console.log(buttons.length);
+  buttons[0].addEventListener('click', function() {
+    console.log("propertied");
+  });
+}
+
+propertiesClicked();
