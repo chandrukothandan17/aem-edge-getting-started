e9a3c46bf6d8b63cc7d318b18be822f0df279dcb
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-20 21:54:05 +0530

    Service

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,8 +1,11 @@
 console.log("hello");
 
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("dom loaded");
     const wrapper = document.getElementsByClassName('service-alert-wrapper'); 
+    console.log(wrapper);
     wrapper.forEach((ele) => {
+        console.log(ele);
         const firstChild = ele.firstElementChild;
         console.log(firstChild);
         firstChild.forEach((list) => {
