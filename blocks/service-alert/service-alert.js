aa2ed1289aa5c9f0ae4b07a62205bf28721f1df4
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-20 21:58:57 +0530

    loading

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,5 +1,7 @@
 console.log("hello");
 
+if (document.readyState != "loading") { console.log("loading done");}
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log("dom loaded");
     const wrapper = document.getElementsByClassName('service-alert-wrapper'); 
