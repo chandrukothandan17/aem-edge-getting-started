bc18023055476bd814991b8d2b82d3645c0f00c9
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-20 21:29:21 +0530

    dfd

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -1,40 +1,5 @@
 console.log("hello");
 
-console.log("path "+window.location.pathname);
-
-const url = 'https://universal-editor-service.experiencecloud.live/details';
-
-const jsonData = {
-  "connections": [
-    {
-      "name": "aemconnection",
-      "protocol": "xwalk",
-      "uri": "https://author-p120465-e1171116.adobeaemcloud.com"
-    }
-  ],
-  "target": {
-    "resource": "urn:aemconnection:/content/aem-edge-getting-started-site/index/service-alerts/jcr:content/root/section",
-    "type": "component",
-    "prop": ""
-  }
-};
-
-fetch(url, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(jsonData),
-})
-.then(response => {
-  if (!response.ok) {
-    throw new Error('Network response was not ok ' + response.statusText);
-  }
-  return response.json();
-})
-.then(data => {
-  console.log('Success:', data);
-})
-.catch(error => {
-  console.error('Error:', error);
+$('button[aria-label="Properties"]').onclick(function(){  
+  console.log("propertied"); 
 });
