4157366e39a90ec577c41bc4d6919dc639489e11
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-21 09:54:11 +0530

    dfd

--- blocks/service-alert/service-alert.js
+++ blocks/service-alert/service-alert.js
@@ -5,8 +5,8 @@ if (document.readyState != "loading") {
 
   console.log("dom loaded");
   const wrapper = document.getElementsByClassName('service-alert-wrapper'); 
-  console.log(wrapper);
-  wrapper.forEach((ele) => {
+  console.log(wrapper[0]);
+  wrapper[0].forEach((ele) => {
       console.log(ele);
       const firstChild = ele.firstElementChild;
       console.log('firstChild');
