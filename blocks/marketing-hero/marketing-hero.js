43e908182b39aea2379b21cffcd1d105fbc5932f
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-28 16:43:09 +0530

    dfd

--- blocks/marketing-hero/marketing-hero.js
+++ blocks/marketing-hero/marketing-hero.js
@@ -1,10 +1,9 @@
 export default async function decorate(block) {
 
     [...block.children].forEach((row) => {
-        
         const label = row.children[0];
-        console.log(label.firstChild);
-
+        console.log(label.firstChild.textContent);
+        
     });
 
 }
