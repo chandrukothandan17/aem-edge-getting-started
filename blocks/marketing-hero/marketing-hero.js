7f3ce9bfeb44c08aa0a69f4e7b2148a06d97d449
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-28 16:11:35 +0530

    sdfd

--- blocks/marketing-hero/marketing-hero.js
+++ blocks/marketing-hero/marketing-hero.js
@@ -1,9 +1,10 @@
 export default async function decorate(block) {
 
     [...block.children].forEach((row) => {
-        console.log(row);
+        
         const label = row.children[0];
-        console.log(label);
+        console.log(label.firstChild);
+
     });
 
 }
