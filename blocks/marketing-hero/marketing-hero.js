55488acaadee0ec71070712aab0cf5f7d4291353
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-11-29 15:54:21 +0530

    eftr

--- blocks/marketing-hero/marketing-hero.js
+++ blocks/marketing-hero/marketing-hero.js
@@ -70,7 +70,7 @@ export default async function decorate(block) {
 
         marketingHeroBlock.append(marketingContainer, arcContainer);
         [...block.children].forEach((row) => {
-            row.remove();
+           // row.remove();
         });
         block.appendChild(marketingHeroBlock);
        console.log(block);
