ce73400bbbab2eb352853cf04b8b574ea8d77c10
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-12-02 13:44:31 +0530

    iage

--- blocks/marketing-hero/marketing-hero.js
+++ blocks/marketing-hero/marketing-hero.js
@@ -1,13 +1,13 @@
 export default async function decorate(block) {
 
-   
-    // console.log(block.querySelector('p'));
- 
-     const rows = block.querySelectorAll(':scope > div');
-     createMarketingBlock(rows);
 
-     function createMarketingBlock(rows){
-        if(rows.length>0){
+    // console.log(block.querySelector('p'));
+
+    const rows = block.querySelectorAll(':scope > div');
+    createMarketingBlock(rows);
+
+    function createMarketingBlock(rows) {
+        if (rows.length > 0) {
             console.log(rows[0].innerText);
             const title = rows[0].innerText;
             const description = rows[1].innerText;
@@ -18,81 +18,90 @@ export default async function decorate(block) {
             const imageList = rows[6].querySelectorAll('img');
             const imageurl = imageList[0].getAttribute('src');
             const smallText = rows[7].querySelectorAll('p');
-           // const smallText = rows[7].innerText;
-           console.log(smallText[0].innerHTML);
             const imageType = rows[8].innerText;
 
             const marketingHeroBlock = document.createElement('div');
-        marketingHeroBlock.className = 'ups-component hero hero-default  has-breadcrumbs';
+            marketingHeroBlock.className = 'ups-component hero hero-default  has-breadcrumbs';
 
-        const marketingContainer = document.createElement('div');
-        marketingContainer.className = 'ups-container';
+            const marketingContainer = document.createElement('div');
+            marketingContainer.className = 'ups-container';
 
-        const upsCard = document.createElement('div');
-        upsCard.className = 'card ups-card';
+            const upsCard = document.createElement('div');
+            upsCard.className = 'card ups-card';
 
-        const upsImage = document.createElement('div');
-        upsImage.className = 'card-img fade-in-up-light';
+            const upsImage = document.createElement('div');
+            upsImage.className = 'card-img fade-in-up-light';
 
-        const image = document.createElement('img');
-        image.className = 'img-fluid';
-        image.src = imageurl;
-        image.loading = 'lazy';
-        image.alt = 'banner';
+            const image = document.createElement('img');
+            image.className = 'img-fluid';
+            image.src = imageurl;
+            image.loading = 'lazy';
+            image.alt = 'banner';
 
-        upsImage.appendChild(image);
+            upsImage.appendChild(image);
 
-        const upsBody = document.createElement('div');
-        upsBody.className = 'card-body fade-in-up-light';
+            const upsBody = document.createElement('div');
+            upsBody.className = 'card-body fade-in-up-light';
 
-        const upsBodyContent = document.createElement('div');
-        upsBodyContent.className = 'card-body-content';
+            const upsBodyContent = document.createElement('div');
+            upsBodyContent.className = 'card-body-content';
 
-        const h1 = document.createElement('h1');
-        h1.textContent  = title;
+            const h1 = document.createElement('h1');
+            h1.textContent = title;
 
-        const cta = document.createElement('a');
-        cta.className = 'ups-cta ups-cta-primary';
-        cta.href = ctaUrl.trim();
-        cta.textContent = ctaButton;
+            const cta = document.createElement('a');
+            cta.className = 'ups-cta ups-cta-primary';
+            cta.href = ctaUrl.trim();
+            cta.textContent = ctaButton;
 
-        const span = document.createElement('span');
-        span.className = 'icon ups-icon-right-arrow';
-        
-        cta.appendChild(span);
+            const secondaryCta = document.createElement('a');
+            secondaryCta.className = "ups-cta ups-cta-secondary";
+            secondaryCta.href = secondaryCtaUrl.trim();
+            secondaryCta.textContent = secondaryCtaButton;
 
-        const p = document.createElement('p');
-        p.textContent = description;
+            const span = document.createElement('span');
+            span.className = 'icon ups-icon-right-arrow';
 
-        const smallp = document.createElement('p');
-        const small = document.createElement('small');
-        small.innerHTML = smallText[0].innerHTML;
-        smallp.appendChild(small);
+            cta.appendChild(span);
 
-        upsBodyContent.append(h1, p, cta, smallp);
+            secondaryCta.appendChild(span);
 
-        upsBody.appendChild(upsBodyContent);
+            const p = document.createElement('p');
+            p.textContent = description;
 
-        upsCard.append(upsImage, upsBody);
-        
-        marketingContainer.appendChild(upsCard);
+            const smallp = document.createElement('p');
+            const small = document.createElement('small');
+            small.innerHTML = smallText[0].innerHTML;
+            smallp.appendChild(small);
+            console.log(imageType);
+            if(imageType === 'image'){
+                upsBodyContent.append(h1, p, cta, smallp);
+            }else if(imageType === 'image-without-small-text-with-secondary-cta'){
+                upsBodyContent.append(h1, p, cta, secondaryCta, smallp);
+            }
 
-        const arcContainer = document.createElement('div');
-        arcContainer.className = 'arc-container';
+            upsBody.appendChild(upsBodyContent);
 
-        arcContainer.innerHTML = `<svg width="1440" class="arc" height="72" viewBox="0 0 1440 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+            upsCard.append(upsImage, upsBody);
+
+            marketingContainer.appendChild(upsCard);
+
+            const arcContainer = document.createElement('div');
+            arcContainer.className = 'arc-container';
+
+            arcContainer.innerHTML = `<svg width="1440" class="arc" height="72" viewBox="0 0 1440 72" fill="none" xmlns="http://www.w3.org/2000/svg">
                      <path d="M-400 176C139.222 -24.794 1028.42 -10.941 1440 13.8751V176L-400 176Z" fill="white"></path>
                   </svg>`;
 
-        marketingHeroBlock.append(marketingContainer, arcContainer);
-        [...block.children].forEach((row) => {
-             //row.remove();
-        });
-        block.appendChild(marketingHeroBlock);
-        console.log(block);
+            marketingHeroBlock.append(marketingContainer, arcContainer);
+            [...block.children].forEach((row) => {
+                //row.remove();
+            });
+            block.appendChild(marketingHeroBlock);
+            console.log(block);
 
         }
 
-        
-     } 
- }
+
+    }
+}
