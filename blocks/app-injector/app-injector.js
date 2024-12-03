fa378583b3aae7b21b8b0e47fc9064cc5cbb6e4d
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-12-03 13:42:26 +0530

    Hi

--- blocks/app-injector/app-injector.js
+++ blocks/app-injector/app-injector.js
@@ -1,36 +1,40 @@
 import {
     loadCSS,
     loadScript
-  } from '../../scripts/aem.js';
+} from '../../scripts/aem.js';
 
 export default async function decorate(block) {
     console.log(block);
     const rows = block.querySelectorAll(':scope > div');
+    const divId = rows[0].innerText.trim();
+    const parentPath = rows[1].innerText.trim();
+    const jsfiles = rows[2].innerText.trim();
+    const cssfiles = rows[3].innerText.trim();
 
-        console.log(rows);
-        const divId = rows[0].innerText.trim();
-        const parentPath = rows[1].innerText.trim();
-        const jsfiles = rows[2].innerText.trim();
-        const cssfiles = rows[3].innerText.trim();
-       
-        const javascriptfiles = jsfiles.split(',');
-        javascriptfiles.forEach((file)=>{
-            console.log(file);
-            loadScriptInsideApp(parentPath+"/js/"+file);
-        });
+    const javascriptfiles = jsfiles.split(',');
+    javascriptfiles.forEach((file) => {
+        loadScriptInsideApp(parentPath + "/js/" + file);
+    });
 
-        const cascadefiles = cssfiles.split(',');
-        cascadefiles.forEach((file)=>{
-            console.log(file);
-            loadCSSInsideApp(parentPath+"/css/"+file);
-        })                
-    
+    const cascadefiles = cssfiles.split(',');
+    cascadefiles.forEach((file) => {
+        loadCSSInsideApp(parentPath + "/css/" + file);
+    });
+
+    [..block.children].forEach((rows)=>{
+        rows.remove();
+    });
+
+    const divElement = document.createElement('div');
+    divElement.id =  divId;
+
+    block.appendChild(divElement);
 }
 
-async function loadScriptInsideApp(href){
+async function loadScriptInsideApp(href) {
     await loadScript(href, { type: 'module' });
 }
 
-async function loadCSSInsideApp(href){
+async function loadCSSInsideApp(href) {
     await loadCSS(href);
 }
