0f3b52d793ce10110540a127e1ff97ab4b3f46e6
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-12-03 13:25:40 +0530

    dfd

--- blocks/app-injector/app-injector.js
+++ blocks/app-injector/app-injector.js
@@ -8,14 +8,23 @@ export default async function decorate(block) {
     [...block.children].forEach((row, i) => {
         console.log(row);
         console.log(row.innerText);
-        const href = row.innerText.trim();
+        const divId = row[0].innerText.trim();
+        const parentPath = row[1].innerText.trim();
+        const jsfiles = row[2].innerText.trim();
+        const cssfiles = row[3].innerText.trim();
+        console.log(parentPath+" -> "+jsfiles+" -> "+cssfiles);
+        let parentPath;
+      /*  if(href.startsWith('/')){
+            parentPath = href;
+        }
+        console.log(parentPath);
         if(href.includes('js')){
-            loadScriptInsideApp(href);
+            loadScriptInsideApp(parentPath+"/js/"+href);
         }
         if(href.includes('css')){
-            loadCSSInsideApp(href);
-        }
-        
+            loadCSSInsideApp(parentPath+"/css/"+href);
+        } */
+                    
     });
 }
 
