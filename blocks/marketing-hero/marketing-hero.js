9307ac8de36d5b171b100e26678ad3286d9f6656
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-12-04 14:53:33 +0530

    sds

--- blocks/marketing-hero/marketing-hero.js
+++ blocks/marketing-hero/marketing-hero.js
@@ -6,6 +6,18 @@ export default async function decorate(block) {
     const autoplay = block.classList.contains('autoplay');
     console.log(autoplay);
 
+
+    if (autoplay) {
+        const observer = new IntersectionObserver((entries) => {
+          if (entries.some((e) => e.isIntersecting)) {
+            observer.disconnect();
+            const playOnLoad = autoplay && !prefersReducedMotion.matches;
+            loadVideoEmbedMarketingHero(block, link, playOnLoad, autoplay);
+          }
+        });
+        observer.observe(block);
+      }
+
     function createMarketingBlock(rows) {
         if (rows.length > 0) {
             const title = rows[0].innerText;
@@ -128,3 +140,105 @@ export default async function decorate(block) {
 
     }
 }
+
+const loadVideoEmbedMarketingHero = (block, link, autoplay, background) => {
+    if (block.dataset.embedLoaded === 'true') {
+      return;
+    }
+    const url = new URL(link);
+  
+    const isYoutube = link.includes('youtube') || link.includes('youtu.be');
+    const isVimeo = link.includes('vimeo');
+  
+    if (isYoutube) {
+      const embedWrapper = embedYoutubeMarketingHero(url, autoplay, background);
+      block.append(embedWrapper);
+      embedWrapper.querySelector('iframe').addEventListener('load', () => {
+        block.dataset.embedLoaded = true;
+      });
+    } else if (isVimeo) {
+      const embedWrapper = embedVimeoMarketingHero(url, autoplay, background);
+      block.append(embedWrapper);
+      embedWrapper.querySelector('iframe').addEventListener('load', () => {
+        block.dataset.embedLoaded = true;
+      });
+    } else {
+      const videoEl = getVideoElementMarketingHero(link, autoplay, background);
+      block.append(videoEl);
+      videoEl.addEventListener('canplay', () => {
+        block.dataset.embedLoaded = true;
+      });
+    }
+  };
+
+  function getVideoElementMarketingHero(source, autoplay, background) {
+    const video = document.createElement('video');
+    video.setAttribute('controls', '');
+    if (autoplay) video.setAttribute('autoplay', '');
+    if (background) {
+      video.setAttribute('loop', '');
+      video.setAttribute('playsinline', '');
+      video.removeAttribute('controls');
+      video.addEventListener('canplay', () => {
+        video.muted = true;
+        if (autoplay) video.play();
+      });
+    }
+  
+    const sourceEl = document.createElement('source');
+    sourceEl.setAttribute('src', source);
+    sourceEl.setAttribute('type', `video/${source.split('.').pop()}`);
+    video.append(sourceEl);
+  
+    return video;
+  }
+
+
+  function embedYoutubeMarketingHero(url, autoplay, background) {
+    const usp = new URLSearchParams(url.search);
+    let suffix = '';
+    if (background || autoplay) {
+      const suffixParams = {
+        autoplay: autoplay ? '1' : '0',
+        mute: background ? '1' : '0',
+        controls: background ? '0' : '1',
+        disablekb: background ? '1' : '0',
+        loop: background ? '1' : '0',
+        playsinline: background ? '1' : '0',
+      };
+      suffix = `&${Object.entries(suffixParams).map(([k, v]) => `${k}=${encodeURIComponent(v)}`).join('&')}`;
+    }
+    let vid = usp.get('v') ? encodeURIComponent(usp.get('v')) : '';
+    const embed = url.pathname;
+    if (url.origin.includes('youtu.be')) {
+      [, vid] = url.pathname.split('/');
+    }
+  
+    const temp = document.createElement('div');
+    temp.innerHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+        <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+        allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
+      </div>`;
+    return temp.children.item(0);
+  }
+
+  function embedVimeoMarketingHero(url, autoplay, background) {
+    const [, video] = url.pathname.split('/');
+    let suffix = '';
+    if (background || autoplay) {
+      const suffixParams = {
+        autoplay: autoplay ? '1' : '0',
+        background: background ? '1' : '0',
+      };
+      suffix = `?${Object.entries(suffixParams).map(([k, v]) => `${k}=${encodeURIComponent(v)}`).join('&')}`;
+    }
+    const temp = document.createElement('div');
+    temp.innerHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+        <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
+        style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+        frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
+        title="Content from Vimeo" loading="lazy"></iframe>
+      </div>`;
+    return temp.children.item(0);
+  }
+  
