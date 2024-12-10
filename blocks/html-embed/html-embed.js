9a27926e10695eb8b06bab36970840203897add4
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-12-10 15:47:18 +0530

    dfd

--- blocks/html-embed/html-embed.js
+++ blocks/html-embed/html-embed.js
@@ -6,11 +6,19 @@ import {
 export default async function decorate(block) {
     const innerContent = block.querySelectorAll(':scope > div > div');
     const htmlCode = innerContent[0].innerHTML.trim();
-    const js = innerContent[1].innerText.trim();
-    const css = innerContent[2].innerText.trim();
+    const jsscript = innerContent[1].innerText.trim();
+    const cssscript = innerContent[2].innerText.trim();
+    const js = innerContent[3].innerText.trim();
+    const css = innerContent[4].innerText.trim();
     loadScriptInsideApp(js);
+    loadCSSInsideApp(css);
+    if(jssscript){
+        document.head.append(jssscript);
+    }
+    if(cssscript){
+        document.head.append(cssscript);
+    }
     const markup = htmlCode.replaceAll('&lt;', '<').replaceAll('&gt;', '>');
-    console.log(markup.getElementsByTagName('script'));
     block.innerHTML = markup;
   }
   
