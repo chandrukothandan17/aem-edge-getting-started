db6fe04f5d92a19ad294484fdb8addb575ad395a
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-12-10 16:19:07 +0530

    scr

--- blocks/html-embed/html-embed.js
+++ blocks/html-embed/html-embed.js
@@ -14,7 +14,7 @@ export default async function decorate(block) {
     loadCSSInsideApp(css);
     const parser = new DOMParser();
 const htmlDoc = parser.parseFromString(jsscript, 'text/html');
-console.log(htmlDoc);
+console.log(htmlDoc.getElementByTagName('script'));
 
     if(jsscript){
         receiveScript(jsscript);
