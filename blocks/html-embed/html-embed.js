d408a93d8680295fb75941e278d2b4c1eb36d797
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-12-10 15:21:48 +0530

    dfdf

--- blocks/html-embed/html-embed.js
+++ blocks/html-embed/html-embed.js
@@ -1,8 +1,15 @@
+import {
+    loadCSS,
+    loadScript
+} from '../../scripts/aem.js';
+
 export default async function decorate(block) {
     const innerContent = block.querySelectorAll(':scope > div > div');
-    console.log(innerContent);
+    console.log(innerContent[0].innerHTML);
+    console.log(innerContent[1].innerText);
     const markup = [...innerContent].map((el) => el.textContent).join('').replaceAll('&lt;', '<').replaceAll('&gt;', '>');
     console.log(markup);
     block.innerHTML = markup;
   }
+  
   
