019475f06f25c2db581306947eb455474489c7fe
chandrukothandan17 <chandrukothandan17@gmail.com>
2024-12-10 15:34:36 +0530

    0

--- blocks/html-embed/html-embed.js
+++ blocks/html-embed/html-embed.js
@@ -9,7 +9,7 @@ export default async function decorate(block) {
     const js = innerContent[1].innerText.trim();
     const css = innerContent[2].innerText.trim();
     loadScriptInsideApp(js);
-    console.log(innerContent[2].getElementsByTagName('script'));
+    console.log(innerContent[0].getElementsByTagName('script'));
     const markup = htmlCode.replaceAll('&lt;', '<').replaceAll('&gt;', '>');
     console.log(markup);
     block.innerHTML = markup;
